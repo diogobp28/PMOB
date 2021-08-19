2c211e88d3025841491c8aa9f288088181abfeb8
diogobp28 <86742425+diogobp28@users.noreply.github.com>
2021-08-18 21:26:01 -0300

    main

---
+++ amain.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'atelapmob.dart';
+import 'alistatelas.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    //home: Lista(),
+    //home: CaixaDeEntrada()
+  ));
+}
