038df0716358bcf2bae1509aa94cebdb4f28ba53
diogobp28 <86742425+diogobp28@users.noreply.github.com>
2021-08-18 21:21:25 -0300

    lista revisada

--- alistatelas.dart
+++ alistatelas.dart
@@ -1,69 +1,70 @@
-import 'package:flutter/atelapmob.dart';
+import 'package:flutter/material.dart';
 
-class _CaixaDeEntrada extends State<CaixaDeEntrada>{
+class Lista extends StatefulWidget {
+  const Lista({Key? key}) : super(key: key);
+
   @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: buildAppBar(),
+  ListaState createState() => ListaState();
+}
 
-    body: buildBody(), //conteúdo do body
+class ListaState extends State<Lista> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //appBar: buildAppBar(),
+      body: buildBody(), //conteúdo do body
     ); // scaffold do return
   }
 
-  buildAppBar(){
+  /*
+  buildAppBar() {
     return AppBar();
   }
-  
-  buildBody(){ // corpo do texto
+  */
+  buildBody() {
+    // corpo do texto
     return ListView(
       children: [
-        buildListStile(
-          urlImagem:"",
-          tipoMensagem:"", 
-          msg:"", 
-          data:"");
+        buildListStyle(
+          tipoMensagem: "NOVO PLANTÃO REGISTRADO", 
+          msg: "PLANTÃO CARDIOLÓGICO, 12H - 00H, quar...", 
+          data: "HOJE, 02/03"),
         
-        buildListStile(
-          urlImagem:"",
-          tipoMensagem:"", 
-          msg:"", 
-          data:"");
+        buildListStyle(
+          tipoMensagem: "MENSAGEM DE GAUGUIN", 
+          msg: "INFELIZMENTE NÃO POSSO TROCAR DE PLANTÃO COM VOCÊ, ESTAREI...", 
+          data: "ONTEM, 01/03"),
+        
+        buildListStyle(
+          tipoMensagem: "[AVISO!] FUNCIONAMENTO DO HOSPITAL", 
+          msg: "DURANTE OS DIAS 26 E 27 DE FEVEREIRO, O HOSPITAL NÃO ESTARÁ FUNC...", 
+          data: "SEXTA, 21/02"),
 
-        buildListStile(
-          urlImagem:"",
-          tipoMensagem:"", 
-          msg:"", 
-          data:"");
+        buildListStyle(
+          tipoMensagem: "NOVO PLANTÃO REGISTRADO", 
+          msg: "PLANTÃO SERVIÇOS CLÍNICOS, 12H - 00H, seg...", 
+          data: "HOJE, 02/03"),
       ],
     );
   }
 
   buildListStyle(
-      {String urlImagem: 'https://image.flaticon.com/icons/png/512/1665/1665731.png'}
-      String tipoMensagem, 
-      String msg, 
-      String data)
-    {
-      return ListTile(
-          
-          //leading: Icon(Icons.account_circle_rounde, size:40),
+      {String urlImagem =
+          'https://image.flaticon.com/icons/png/512/1665/1665731.png'},
+      String tipoMensagem,
+      String msg,
+      String data) {
+    return ListTile(
+      
+      //leading: Icon(Icons.account_circle_rounde, size:40),
+      leading:
+          CircleAvatar(backgroundImage: NetworkImage(urlImagem), radius: 20),
 
-          leading: CircleAvatar(backgroundImage: NetworkImage(urlImagem), radius: 20),
-          
-          title: 
-          Text(
-            tipoMensagem, 
-            style: TextStyle(
-              fontWeight.bold, 
-              font size:16)
-            ),
-          
-          subtitle: Text(msg),
-          
-          trailing: 
-            Text(data, 
-            style: 
-              TextStyle(color: Colors.blue)), 
-      ); // listas
-    }
+      title: Text(tipoMensagem, style: TextStyle()),
+
+      subtitle: Text(msg),
+
+      trailing: Text(data, style: TextStyle(color: Colors.blue)),
+    ); // listas
+  }
 }
